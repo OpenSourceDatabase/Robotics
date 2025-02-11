6067253e14ca79149cffe3d79080a8cd1d4645db
Enric Cervera <ecervera@uji.es>
2025-02-11 16:49:28 +0100

    Create script

---
+++ publication-server/import_csv_files.sql
@@ -0,0 +1,9 @@
+.mode csv
+.import --skip 1 ../Journals/TRO/processed_2020-2024.csv publications_publication
+.import --skip 1 ../Journals/TASE/processed_2018-2024.csv publications_publication
+.import --skip 1 ../Journals/RAM/processed_2021-2024.csv publications_publication
+.import --skip 1 ../Journals/RAL/processed_2016-2024.csv publications_publication
+.import --skip 1 ../Conferences/IROS/processed_2013-2024.csv publications_publication
+.import --skip 1 ../Conferences/CASE/processed_2016-2024.csv publications_publication
+.import --skip 1 ../Conferences/ICRA/processed_2016-2024.csv publications_publication
+
