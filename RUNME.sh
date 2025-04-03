1bc57ecd79fa94c1f2a40e476cfce7d3bb20b9c6
Enric Cervera <ecervera@uji.es>
2025-04-03 14:32:37 +0200

    Check prerequisite

--- RUNME.sh
+++ RUNME.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+if ! docker -v &>/dev/null; then
+  echo "Prerequisite error: Docker not found"
+  exit 1
+fi
+
 docker run --rm -it -p 8000:8000 ghcr.io/opensourcedatabase/robotics:main
 
